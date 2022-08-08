e568162099f8bc39a3409cfedc01c3e5a01b8ac1
rawheel <raheeelalisiddiqui@gmail.com>
2022-08-09 04:25:18 +0500

    Added vuex and font awesome

--- src/main.js
+++ src/main.js
@@ -3,14 +3,36 @@ import App from './App.vue'
 import './index.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+
+
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import specific icons */
+import { faUser} from '@fortawesome/free-solid-svg-icons'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(faUser)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+Vue.use(Vuesax, {
+  // options here
+})
 
 Vue.config.productionTip = false
 
